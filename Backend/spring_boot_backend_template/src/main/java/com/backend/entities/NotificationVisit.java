fa42426d8c54649e297a7b1c7e2b7cb8f6fe3dd0
pavanthorat57314 <pavanthorat57314@gmail.com>
2026-01-27 01:06:07 +0530

    Did Changes in the notification_visit entity

--- Backend/spring_boot_backend_template/src/main/java/com/backend/entities/NotificationVisit.java
+++ Backend/spring_boot_backend_template/src/main/java/com/backend/entities/NotificationVisit.java
@@ -1,16 +1,15 @@
 package com.backend.entities;
 
-
-
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class NotificationVisit{
+public class NotificationVisit {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,9 +19,14 @@ public class NotificationVisit{
 
     private boolean readFlag = false;
 
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime createdAt;
 
     @ManyToOne
     private User user;
-}
 
+  
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
